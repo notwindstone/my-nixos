5b504944be6d0eee93c6f55fbbd4e28a17dc28d9
notwindstone <notwindstone@gmail.com>
2025-12-13 23:53:40 +0300

    feat: use xanmod kernel

--- boot.nix
+++ boot.nix
@@ -1,7 +1,11 @@
+{ pkgs, ... }:
 {
   # Defaults
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Xanmod Kernel
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
   # Hide Stage 2 and further logs behind the NixOS logo
   boot.plymouth.enable = true;
@@ -9,4 +13,8 @@
   # Hide most of the log messages
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+  ];
 }
