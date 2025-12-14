3554c0240f16185cfbc6f47789775917d165afda
notwindstone <notwindstone@gmail.com>
2025-12-14 23:44:40 +0300

    feat: add shells

---
+++ shells/tauri.nix
@@ -0,0 +1,36 @@
+# Run with `nix-shell shell.nix`
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    rustc
+    nodejs
+    bun
+    cargo
+    cargo-tauri
+    pkg-config
+    cmake
+    wrapGAppsHook3
+    llvmPackages.libllvm
+    llvmPackages.lld
+    llvmPackages.clang
+  ];
+
+  buildInputs = with pkgs;[
+    webkitgtk_4_1
+    glib
+    gsettings-desktop-schemas
+    dbus
+    openssl_3
+    cairo
+    gdk-pixbuf
+  ];
+  shellHook = ''
+    export GDK_BACKEND="x11"
+  '';
+  # export WEBKIT_DISABLE_DMABUF_RENDERER=1
+  # export GIO_MODULE_DIR=${pkgs.glib-networking}/lib/gio/modules/
+  # '';
+}
+
